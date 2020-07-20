5df1a920a3da43f3365f7b9e5af5eb01c43b962d
Connor Runyan <connor.runyan@aggiemail.usu.edu>
2020-07-19 22:23:06 -0700

    Added comment about redundant if with -e flag

--- run_linux_version.sh
+++ run_linux_version.sh
@@ -9,6 +9,7 @@ cmake ..
 make
 
 # Execute tests. If they pass, run the simulation
+# This if statement is a little redundant with the -e flag
 ./UnitTestRunner
 if [ $? -eq 0 ]; then
     ./ConwaysLife
